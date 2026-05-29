86b8b450a58a1aa790f121876a485488cbfde800
KenSumiyoshi <kensumiyoshi1987@gmail.com>
2026-05-28 22:02:38 -0400

    Block 3.01 Workshop: Secure the Vault

---
+++ index.js
@@ -0,0 +1,13 @@
+let code1;
+code1 = 7 + 3;
+
+const code2 = 100 / 2 - 10;
+console.log(code2);
+const code3 = (10 % 3) + 38;
+console.log(code3);
+const message = "The vault has been secured. The combination is:";
+console.log(message);
+const codeA = code1 + "-" + code2 + "-" + code3;
+const codeB = `${code1}-${code2}-${code3}`;
+console.log(codeA);
+console.log(codeB);
